cd69f05497a6bdb7dabe1128bd54fc6ad0f403d1
Guilherme Gomes <ggomes.jua.ti@gmail.com>
2025-02-02 16:44:28 -0300

    :tada: init: First commit with initial project structure

---
+++ api-encryption/src/main/java/com/ggomes/api_encryption/ApiEncryptionApplication.java
@@ -0,0 +1,13 @@
+package com.ggomes.api_encryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEncryptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEncryptionApplication.class, args);
+	}
+
+}
